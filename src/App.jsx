e7c89d99ae045da9aa69a4738e8cca7b0d8d5343
kruti-patel2815 <krutipatel2811.cdmi@gmail.com>
2025-06-16 13:42:48 +0530

    project

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,28 @@ import { FaPhone } from "react-icons/fa";
 import { FaLocationArrow } from "react-icons/fa";
 import { FaHeadphonesAlt } from "react-icons/fa";
 import { FaAngleDown } from "react-icons/fa";
-
+import { FaSearch } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa";
+import { FaRegCommentAlt } from "react-icons/fa";
+import Logo from './image/logo_black (1).png';
+import React from "react";
+import Slider from "react-slick";
+import image from "./image/bg-img-01.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+    
+  };
   return (
     <>
       <header>
@@ -40,8 +60,8 @@ function App() {
         <Navbar className='' class='header-bottom'>
           <Container>
             <Row style={{ alignItems: "center" }}>
-              <Col style={{textAlign: "center" }}>
-                <ul class='flex padding' >
+              <Col style={{ textAlign: "center" }}>
+                <ul className='flex' >
                   <li class='nav-item'>HOME<FaAngleDown /></li>
                   <li class='nav-item'>SHOP<FaAngleDown /></li>
                   <li class='nav-item'>PRODUCT<FaAngleDown /></li>
@@ -49,14 +69,50 @@ function App() {
                   <li class='nav-item'>PAGES<FaAngleDown /></li>
                 </ul>
               </Col>
-              <Col style={{ textAlign: "center" }}>2</Col>
-              <Col style={{ textAlign: "center" }}>3</Col>
+              <Col style={{ textAlign: "center" }}>
+                <img src={Logo} alt="Logo" style={{ width: "43%", height: "40px" }} />
+
+
+              </Col>
+              <Col style={{ textAlign: "center" }}>
+                <ul className='flex' >
+                  <li class='nav-item'>USD<FaAngleDown /></li>
+                  <li class='nav-item'>ENGLISH<FaAngleDown /></li>
+                  <li class='nav-item'><FaSearch /></li>
+                  <li class='nav-item'><FaRegUser /></li>
+                  <li class='nav-item'><FaRegComment /></li>
+                  <li class='nav-item'><FaRegCommentAlt /></li>
+
+                </ul>
+              </Col>
 
             </Row>
           </Container>
 
         </Navbar>
+
       </header>
+      <hero>
+
+        <div className="slider-container">
+          <Slider {...settings}>
+            <div>
+              <h3><img src={image} alt="" style={{width:"100%"}} /></h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            
+          </Slider>
+        </div>
+
+
+
+
+      </hero>
 
 
 
